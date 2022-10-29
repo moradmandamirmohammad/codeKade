a6e66a66eec656186609df25bc4ec33f7bef295d
moradmandamirmohammad <moradmandamirmohammad@gmail.com>
2022-10-29 12:07:11 +0330

    delete some item in project

--- codeKade/codeKade.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace codeKade.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
