0b52d88ecfaff2c4279a8deb346db84a37b4f3fb
Yoones Asgarian <yoonesasgasg@gmail.com>
2022-11-16 10:51:26 +0330

    add area of user

--- codeKade/codeKade.Web/Program.cs
+++ codeKade/codeKade.Web/Program.cs
@@ -87,6 +87,14 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "areas",
+        pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    );
+});
+
 SiteCurrentContext.Configure(((IApplicationBuilder)app).ApplicationServices.GetRequiredService<IHttpContextAccessor>());
 
 //app.MapRazorPages();
