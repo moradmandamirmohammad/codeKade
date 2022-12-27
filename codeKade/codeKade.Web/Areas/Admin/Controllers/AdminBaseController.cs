8d2b056acf257d4bfae70cdc975e888f70905cd9
Yoones Asgarian <yoonesasgasg@gmail.com>
2022-12-27 20:48:29 +0330

    add area for admin

---
+++ codeKade/codeKade.Web/Areas/Admin/Controllers/AdminBaseController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace codeKade.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin")]
+    public class AdminBaseController : Controller
+    {
+    }
+}
